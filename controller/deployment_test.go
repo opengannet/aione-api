8aa5f177fd0153fd8cbef56c7fd703de909d5401
kevin <kevinj@gmail.com>
2026-08-12 22:14:10 +0800

    test: use project SQLite driver

--- controller/deployment_test.go
+++ controller/deployment_test.go
@@ -11,9 +11,9 @@ import (
 	"github.com/QuantumNous/new-api/model"
 	"github.com/QuantumNous/new-api/pkg/flyte2"
 	"github.com/gin-gonic/gin"
+	"github.com/glebarez/sqlite"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
